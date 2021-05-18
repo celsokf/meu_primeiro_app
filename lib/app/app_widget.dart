91907ca890821fe71acb5ff4d03fa03d51574073
Celso Fujii <celsokf@gmail.com>
2021-05-17 23:44:28 -0300

    feat: Create counter app

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meu_primeiro_app/app/pages/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Meu primeiro app",
+      home: HomePage(),
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        scaffoldBackgroundColor: Colors.purpleAccent,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.purpleAccent,
+        ),
+      ),
+      darkTheme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.grey,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.grey,
+        ),
+      ),
+    );
+  }
+}
